557a3e6ea4a6974cf8149726c536563f6d9038cb
Florence <florencesytsang@gmail.com>
2024-11-16 16:35:31 +0800

    Button to clear all record.

--- src/dashboard/ControlPanel.js
+++ src/dashboard/ControlPanel.js
@@ -2,6 +2,7 @@ import React, {useRef} from 'react';
 import Button from "@mui/material/Button";
 import {Box} from "@mui/material";
 import exportFromJSON from "export-from-json";
+import { BOOKS } from '../bible/constants.ts';
 
 export default function ControlPanel(props) {
 
@@ -28,14 +29,32 @@ export default function ControlPanel(props) {
         }
     };
 
-    const handleLoad = (e) => {
+    const handleLoad = () => {
         if (!inputRef || !inputRef.current) return;
         inputRef.current.click();
     };
 
+    const handleClearAll= () => {
+        console.log("Clearing all");
+        const readStatus = {}
+        BOOKS.map(book => {
+            for (let i = 1; i < book.numChapters+1; i++) {
+                const chapterKey = book.name+"_"+i;
+                // readStatus[chapterKey] = false;
+                localStorage.setItem(chapterKey, "false");
+            }
+        })
+        console.log(readStatus);
+        setReadStatus(readStatus);
+    };
+
+
     return (
         <div>
-            {/*<Button variant={"contained"}>Clear All</Button>*/}
+            <Button variant={"contained"} onClick={handleClearAll}>Clear All</Button>
+            <Box sx={{
+                flex: '0 0 32px'
+            }}></Box>
             <Button variant={"contained"} onClick={handleSave}>Save bible study record</Button>
             <Box sx={{
                 flex: '0 0 32px'
